8082b8574121fe1836bfa346d574147cdc551ee2
akshay <akshaysharma5432@gmail.com>
2023-05-21 13:06:19 -0400

    fetch games data

--- Game-Fusion/src/App.tsx
+++ Game-Fusion/src/App.tsx
@@ -1,5 +1,6 @@
 import { Button, ButtonGroup, Grid, GridItem, Show } from '@chakra-ui/react';
 import NavBar from './components/NavBar';
+import GameGrid from './components/GameGrid';
 
 function App() {
 
@@ -13,7 +14,7 @@ function App() {
         <Show above='lg'>
         <GridItem area="aside" bg="gold">Aside</GridItem>
         </Show>
-        <GridItem area="main" bg="dodgerBlue">Main</GridItem>
+        <GridItem area="main" bg="dodgerBlue"><GameGrid /></GridItem>
       </Grid>
       </div>
   )
