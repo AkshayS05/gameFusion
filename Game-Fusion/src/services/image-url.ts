99ba6134888b98e679d2a2211a10a408c5b776e8
akshay <akshaysharma5432@gmail.com>
2023-05-23 16:31:04 -0400

    handle games with no image

--- Game-Fusion/src/services/image-url.ts
+++ Game-Fusion/src/services/image-url.ts
@@ -1,5 +1,10 @@
-const getCroppedImageUrl =(url : string) =>{
-  if( !url ) return '';
+import noImage from '../assets/no-image.webp';
+
+const getCroppedImageUrl = (url : string) =>{
+
+  if( !url ) return noImage;
+
+
   const target = ('media/')
   //make the image sizes smaller by cropping them. The coprring path is already provided in the api
   const index = url.indexOf(target) + target.length;
