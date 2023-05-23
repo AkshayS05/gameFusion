3cf2b20b7e438f31364bb1168225c5fbded0127a
akshay <akshaysharma5432@gmail.com>
2023-05-23 16:35:30 -0400

    fix the issue with chakra menus

--- Game-Fusion/src/App.tsx
+++ Game-Fusion/src/App.tsx
@@ -1,4 +1,4 @@
-import { Button, ButtonGroup, Grid, GridItem, HStack, Show } from '@chakra-ui/react';
+import { Box, Button, ButtonGroup, Flex, Grid, GridItem, HStack, Show } from '@chakra-ui/react';
 import NavBar from './components/NavBar';
 import GameGrid from './components/GameGrid';
 import GenreList from './components/GenreList';
@@ -45,10 +45,12 @@ const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
         {/* passing selected genre to the child in order to display selected genre only. */}
         
         <GridItem area="main" >
-          <HStack spacing={5} paddingLeft={2} marginBottom={5}>
+          <Flex paddingLeft={2} marginBottom={5}>
+            <Box marginRight={5}>
           <PlatformSelector selectedPlatform ={gameQuery.platform} onSelectPlatform={(platform)=> setGameQuery({...gameQuery,platform})}/>
+            </Box>
           <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({...gameQuery, sortOrder})}/>
-          </HStack>
+          </Flex>
           <GameGrid gameQuery={gameQuery} /></GridItem>
       </Grid>
       </div>
